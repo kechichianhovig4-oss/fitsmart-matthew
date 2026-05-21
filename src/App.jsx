8b1f52295976c67f69c92343401dc1f95e14ae88
Kechichian Hovig <kechichianhovig4@gmail.com>
2026-05-21 15:15:20 -0400

    fix: updated import path to lowercase p Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./components/HomePage";
+import HomePage from "./components/Homepage";
 import Services from "./components/Services";
 import Contact from "./components/Contact";
 import About from "./components/AboutMe";
